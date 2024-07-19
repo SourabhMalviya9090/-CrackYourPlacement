29bb2e5c38137b6fd85eb59fd3769896375b2c17
SourabhMalviya9090 <142317647+SourabhMalviya9090@users.noreply.github.com>
2024-07-19 13:08:29 +0530

    Update simplifyPath.cpp

--- simplifyPath.cpp
+++ simplifyPath.cpp
@@ -1,1 +1,39 @@
 
+class Solution {
+public:
+    string simplifyPath(string path) {
+        int ind =0;
+        stack<string> st;
+        while(ind<path.size()){
+            string curr ="";
+            while(ind<path.size() && path[ind]=='/'){
+                ind++;
+            }
+            curr+= '/';
+            while(ind<path.size() && path[ind]!='/'){
+                curr+= path[ind];
+                ind++;
+            }
+
+            if(curr!="/." && curr!="/.." && curr!="/"){
+                st.push(curr);
+            }
+
+            else if(curr=="/.."){
+                if(st.empty()!=1 && st.top()!="/"){
+                    st.pop();
+                }
+            }
+
+        }
+
+        string ans  ="";
+        while(st.empty()!=1){
+            ans = st.top()+ ans;
+            st.pop();
+        }
+        if(ans=="") ans ="/";
+        return ans;
+        
+    }
+};
